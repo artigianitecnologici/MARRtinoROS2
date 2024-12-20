ca92aed6e76aa9364874bd8da9ae899cde686d68
Fabio Ferrarini <ferrarini09@gmail.com>
2024-12-20 19:24:43 +0100

    V.1.0.0

--- marrtinorobot2/marrtinorobot2_webinterface/marrtinorobot2_webinterface/websocket_robot.py
+++ marrtinorobot2/marrtinorobot2_webinterface/marrtinorobot2_webinterface/websocket_robot.py
@@ -1,4 +1,23 @@
+# Copyright 2025 robotics-3d.com 
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Author: Ferrarini Fabio
+# Email : ferrarini09@gmail.com
+#
 #!/usr/bin/env python3
+
+import rclpy
 
 import os
 import time
@@ -128,9 +147,10 @@ def run_code(code):
     status = "Executing program"
     exec_thread(code)
     status = "Idle"
-
+ 
 if __name__ == "__main__":
     os.makedirs(logdir, exist_ok=True)
+    os.environ['ROS_LOG_DIR'] = '/tmp/ros_logs'
 
     # Robot initialization
     rclpy.init()
