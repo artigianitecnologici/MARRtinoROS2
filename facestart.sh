44635bcaded52b9e8c23b4bfed004bb895c6c940
Fabio Ferrarini <ferrarini09@gmail.com>
2025-01-13 06:42:11 +0100

    Update

--- facestart.sh
+++ facestart.sh
@@ -1,4 +1,5 @@
 #!/bin/sh 
 # echo "Launch Browser"
 #DISPLAY=:0 midori -p -e Fullscreen -a http://localhost:8080/social/marrtina.html 
+rm /home/marrtino/snap/chromium/common/chromium/SingletonLock 
 chromium-browser --app=http://localhost/social/marrtina04.html   -start-maximized
