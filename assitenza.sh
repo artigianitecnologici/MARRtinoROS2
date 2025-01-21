2657039bfbe09cec18b1af82b8920198da7dcf15
Fabio Ferrarini <ferrarini09@gmail.com>
2025-01-21 07:38:09 +0100

    Delete assitenza.sh

--- assitenza.sh
+++
@@ -1,5 +0,0 @@
-sudo sshpass -p 'PwdMrt2022@' ssh -N \
-    -R 51.75.22.208:8022:localhost:22 \
-    -R 51.75.22.208:8080:localhost:80 \
-    -R 51.75.22.208:8085:localhost:8085 \
-    marrtino@51.75.22.208 -o "StrictHostKeyChecking no"
